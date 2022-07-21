b89f2bcae39411d9f0fa9c2f3d89507faa032264
selvavl <87084242+selvavl@users.noreply.github.com>
2022-07-21 00:16:45 -0500

    Update app.js
    Operation object changed to handle more info

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const main = document.querySelector("#main");
 
 let operation = {
     displayNumber: "0",
-    memoryNumber: "",
+    memory: ["", ""],
     operator: "",
     checkerZero: false,
     checkerOperator: false,
@@ -59,9 +59,10 @@ let operation = {
 
 function newOperation() {
     operation.displayNumber = "0";
-    operation.memoryNumber = "";
+    operation.memory = ["", ""];
     operation.operator = "";
-
+    operation.checkerZero = false,
+    operation.checkerOperator = false,
     main.innerText = operation.displayNumber;
 }
 
@@ -69,9 +70,10 @@ function buttonListener(){
     buttons.forEach(button => button.addEventListener("click", function() {
 
         if(button.classList.contains("number")) {
+            
             if(operation.operator != "") {      //if a number is pressed and an operator is present then:
                 if(operation.checkerOperator == false) {    //first, the display number must move into memory and display must be cleared
-                    operation.memoryNumber = operation.displayNumber;
+                    operation.memory[0] = operation.displayNumber;
                     operation.displayNumber = "";
                     operation.checkerOperator = true;       //to make this clear function to happen once add a checker
                 }
@@ -80,19 +82,23 @@ function buttonListener(){
             if(this.innerText == "0" && operation.displayNumber == "0") {        //if 0 is pressed while 0 is in display, do nothing
                 console.log("do nothing!");
             } else {
+
                 if(operation.checkerZero == false) { //when a number that differs from 0 is pressed, erase 0 in display value, a checker is need to be done just once.
                     operation.displayNumber = "";
                     operation.checkerZero = true;
                 }
                 operation.displayNumber += button.innerText;
                 main.innerText = operation.displayNumber;
+
             }
+            
+            
         }
         
         if(button.classList.contains("operator")) {
-            if(operation.displayNumber != "" && operation.memoryNumber != "") { //when an operator is pressed and there are numbers in display and memory, operate
-                operation.displayNumber = operate(operation.memoryNumber, operation.displayNumber, operation.operator);
-                operation.memoryNumber = ""; //memory then is cleaned  
+            if(operation.memory[0] != "") { //when an operator is pressed and there are numbers in display and memory, operate
+                operation.displayNumber = operate(operation.memory[0], operation.displayNumber, operation.operator);
+                operation.memory[0] = ""; // memory then is cleaned  
                 operation.operator = "";    //operator is also cleaned
                 operation.checkerOperator = false;  //operator checker is cleared
                 main.innerText = operation.displayNumber;
@@ -100,6 +106,7 @@ function buttonListener(){
             operation.operator = this.innerText;
             operator.innerText = operation.operator;
         }
+
 
         if(button.classList.contains("posneg")) {
             operation.displayNumber *= -1;
@@ -124,28 +131,7 @@ function buttonListener(){
 
         }
     
-        // if (button.classList.contains("operator")) {
-        //     if(operation.memoryNumber != 0) {
-        //         operation.displayNumber = operate(operation.memoryNumber,operation.displayNumber,operation.operator);
-        //         main.innerText = operation.displayNumber;
-        //     } else{
-        //         operation.operator = button.innerText;  //Display and assign Operator 
-        //         operator.innerText = operation.operator;
-        //         operation.memoryNumber = operation.displayNumber;
-        //         operation.displayNumber = "";
-        //     }
-            
-        
-        // } else if(button.classList.contains("number")) {   //Display and assign displayNumber
-        //     operation.displayNumber += button.innerText;
-        //     main.innerText = operation.displayNumber;
-    
-        // } else if (button.id == "posneg") {
-        //     operation.displayNumber = String(operation.displayNumber * -1); //change positive or negative value to displayNumber
-        //     main.innerText = operation.displayNumber;
-        // }
-    
-        // console.log(operation);
+
     }))
         
 }
